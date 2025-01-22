4a0346b0d6a1da5892a4e3584199dc17dc731dc5
Brian Tum <brian.tum10@yahoo.com>
2025-01-22 12:00:58 +0300

    start of watch locations

---
+++ src/watchLocations.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  watchLocations.c
+ *
+ *    Description: The dir one wants to watch, the files and sub dir are stored in sqlite
+ *    		   table as paths
+ *        Version:  1.0
+ *        Created:  01/22/2025 10:17:24 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
